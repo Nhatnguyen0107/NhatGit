3d06cce1a067add86ebdad63a66131efc345920f
Nhatnguyen0107 <nguyenquocnhat2005@gmail.com>
2025-11-18 14:28:07 +0700

    s

--- frontend/src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as HomePage } from './HomePage';
-export { default as ProductsPage } from './ProductsPage';
-export { default as LoginPage } from './LoginPage';
-export { default as RegisterPage } from './RegisterPage';
